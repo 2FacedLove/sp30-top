f820a1c6939e2fb906ff810ea704652ca5247816
Remo <rimonl@gmail.com>
2014-09-23 19:17:46 +0300

    vpd ac2dc and faster boot in SD

--- target/add-ons/fs/usr/local/bin/boxtype.sh
+++ target/add-ons/fs/usr/local/bin/boxtype.sh
@@ -6,38 +6,81 @@ MODEL_ID="SP1x"
 MODEL_NAME="SP10"
 RC=10
 
-if [ ! "${SMB}" == "SMB-" ] ; then
-	echo "BAD VPD"
-	exit 1
-fi
+proc_box_smb() {
+	if [ ! "${SMB}" == "SMB-" ] ; then
+#		echo "BAD VPD"
+		RC=1
+	elif [ "${PNR}" == "1200" ] ; then
+		#echo "SMB-${PNR} - SP10"
+		MODEL_ID=SP1x
+		MODEL_NAME=SP10
+		RC=10
+	elif  [ "${PNR}" == "1500" ] ; then
+		#echo "SMB-${PNR} - SP2x/SP20"
+		MODEL_ID=SP2x
+		MODEL_NAME=SP20
+		RC=20
+	elif  [ "${PNR}" == "5400" ] ; then
+		#echo "SMB-${PNR} - SP3x/SP30"
+		MODEL_ID=SP3x
+		MODEL_NAME=SP30
+		RC=30
+	elif  [ "${PNR}" == "5431" ] ; then
+		#echo "SMB-${PNR} - SP3x/SP31"
+		MODEL_ID=SP3x
+		MODEL_NAME=SP31
+		RC=31
+	elif  [ "${PNR}" == "5435" ] ; then
+		#echo "SMB-${PNR} - SP3x/SP35"
+		MODEL_ID=SP3x
+		MODEL_NAME=SP35
+		RC=35
+	fi
+}
 
-if [ "${PNR}" == "1200" ] ; then
-	#echo "SMB-${PNR} - SP10"
-	MODEL_ID=SP1x
-	MODEL_NAME=SP10
+proc_mng_ela() {
+	VPD=`readmngvpd.sh`
+	ELA=${VPD:12:4}
+	PNR=${VPD:16:4}
+	MODEL_ID="SP1x"
+	MODEL_NAME="SP10"
 	RC=10
-elif  [ "${PNR}" == "1500" ] ; then
-	#echo "SMB-${PNR} - SP2x/SP20"
-	MODEL_ID=SP2x
-	MODEL_NAME=SP20
-	RC=20
-elif  [ "${PNR}" == "5400" ] ; then
-	#echo "SMB-${PNR} - SP3x/SP30"
-	MODEL_ID=SP3x
-	MODEL_NAME=SP30
-	RC=30
-elif  [ "${PNR}" == "5431" ] ; then
-	#echo "SMB-${PNR} - SP3x/SP31"
-	MODEL_ID=SP3x
-	MODEL_NAME=SP31
-	RC=31
-elif  [ "${PNR}" == "5435" ] ; then
-	#echo "SMB-${PNR} - SP3x/SP35"
-	MODEL_ID=SP3x
-	MODEL_NAME=SP35
-	RC=35
-fi
 
+	if [ ! "${ELA}" == "ELA-" ] ; then
+#		echo "BAD VPD"
+		RC=1
+	elif [ "${PNR}" == "0002" ] ; then
+		#echo "ELA-${PNR} - SP10"
+		MODEL_ID=SP1x
+		MODEL_NAME=SP10
+		RC=10
+	elif  [ "${PNR}" == "1002" ] ; then
+		#echo "ELA-${PNR} - SP2x/SP20"
+		MODEL_ID=SP2x
+		MODEL_NAME=SP20
+		RC=20
+	elif  [ "${PNR}" == "2002" ] ; then
+		#echo "SMB-${PNR} - SP3x/SP30"
+		MODEL_ID=SP3x
+		MODEL_NAME=SP30
+		RC=30
+	elif  [ "${PNR}" == "5431" ] ; then
+		#echo "ELA-${PNR} - SP3x/SP31"
+		MODEL_ID=SP3x
+		MODEL_NAME=SP31
+		RC=31
+	elif  [ "${PNR}" == "3002" ] ; then
+		#echo "ELA-${PNR} - SP3x/SP35"
+		MODEL_ID=SP3x
+		MODEL_NAME=SP35
+		RC=35
+	fi
+}
+
+proc_box_smb
+if [ ${RC} -eq 1 ] ; then
+	proc_mng_ela
+fi
 
 echo "${MODEL_NAME}"
 
